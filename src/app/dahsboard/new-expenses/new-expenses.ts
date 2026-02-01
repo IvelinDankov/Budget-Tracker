ad0e8d98f4ec795a0f09c198150964c5af20350b
Velin Dankov <velin80@web.de>
2026-02-01 10:08:42 +0200

    adding addIncome and addExpense dialogs.

---
+++ src/app/dahsboard/new-expenses/new-expenses.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-expenses',
+  imports: [],
+  templateUrl: './new-expenses.html',
+  styleUrl: './new-expenses.scss',
+})
+export class NewExpenses {
+
+}
---
+++ src/app/dahsboard/new-income/new-income.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-income',
+  imports: [],
+  templateUrl: './new-income.html',
+  styleUrl: './new-income.scss',
+})
+export class NewIncome {
+
+}
